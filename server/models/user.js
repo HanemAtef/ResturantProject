b2257fa1d2d6c057859e09399c900bf1a1f2b2de
Khloud Ahmed <khloudabdelkawy24@gmail.com>
2026-05-06 14:09:35 +0300

    .

---
+++ server/models/user.js
@@ -0,0 +1,43 @@
+const mongoose=require ("mongoose");
+const userschema= new mongoose.Schema({
+    name:{
+        type:String,   
+        required:true ,
+         trim: true,
+      minlength: [3, "Username must be at least 3 characters"],
+      maxlength: [50, "Username cannot exceed 50 characters"]
+            
+    },
+    email:{
+        type:String,        
+        required:true,
+        unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"]
+    },
+    password:{
+        type:String,        
+        required:true,
+        trim : true,
+        minlength: [6, "Password must be at least 6 characters"],
+        maxlength: [100, "Password cannot exceed 100 characters"]
+    },  
+   phone:{
+        type:String,
+        required:true,
+        trim: true,
+        match: [/^\d{11}$/, "Please enter a valid 11-digit phone number"]
+   },
+     role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    },
+    address:{
+        type:String,
+        required:true,
+       
+    }
+},{timestamps:true})
+
+const User= mongoose.model("User",userschema);
+module.exports=User;
