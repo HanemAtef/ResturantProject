79750e21819764404ebb5076360a7d24366f57df
Khloud Ahmed <khloudabdelkawy24@gmail.com>
2026-05-20 13:34:05 +0300

    .

--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -1,6 +1,4 @@
 import styles from "../Menu/Menu.module.css";
-import Navbar from '../../components/layouts/Navbar/Navbar' ;
-import Footer from '../../components/layouts/Footer/Footer';
 import Searchbar from "../../components/Menu/searchmenu/Searchbar" ;
 import Dishes from "../../components/Menu/dishes/Dishes";
 import { useState } from "react";
@@ -10,7 +8,7 @@ function Menu(){
 
     return(
         <>
-        <Navbar/>
+      
         <div className={styles.address}> 
             <h1>Our Curated Menu</h1>
             <p>Discover the finest urban dining experience, crafted with passion and seasonal
@@ -18,7 +16,7 @@ function Menu(){
         </div>
         <Searchbar searchTerm={searchTerm} onSearch={setSearchTerm}/>
         <Dishes searchTermFromParent={searchTerm}/>
-        <Footer/>
+      
         </>
     )
 }
